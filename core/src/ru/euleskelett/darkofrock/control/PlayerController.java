483c15456c2f93a786a3d9a3e582cbfd3d44d6c4
EuleSkelett <64166286+EuleSkelett@users.noreply.github.com>
2020-04-24 21:39:08 +0300

    Перемещение игрока

--- core/src/ru/euleskelett/darkofrock/control/PlayerController.java
+++ core/src/ru/euleskelett/darkofrock/control/PlayerController.java
@@ -1,43 +1,55 @@
 package ru.euleskelett.darkofrock.control;
-
+/*
+* Управление персонажем
+* */
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Polygon;
 import ru.euleskelett.darkofrock.view.GameScreen;
 
-
 public class PlayerController {
-    private float playerSpeed, velosity = 10f, speedMax = 10f;
-    private float rotationSpeed = 30f;
+    private float playerSpeed, velosity = 30f, speedMax = 30f;
 
     private Polygon playerBounds;
     public PlayerController(Polygon playerBounds){
         this.playerBounds = playerBounds;
     }
 // все что связанно со скоростью
-
-
     public void handle(){
         // движение вперед назад
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
-            playerSpeed += velosity* GameScreen.deltaCff;
-        else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
             playerSpeed -= velosity* GameScreen.deltaCff;
+        else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            playerSpeed += velosity* GameScreen.deltaCff;
         else
-            downSpeed();
+            quenchSpeed();
 
-        playerSpeed = sliceSpeed();
+        playerSideMovement();
+
 
         if (Gdx.input.isKeyPressed(Input.Keys.UP))
-            playerBounds.setPosition(playerBounds.getX(), 1);
+            playerSpeed += velosity* GameScreen.deltaCff;
+        else if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
+            playerSpeed -= velosity* GameScreen.deltaCff;
+        else
+            quenchSpeed();
         //
+        playerMovement();
+        playerSpeed = sliceSpeed();
 
-        playerBounds.setPosition(playerBounds.getX() + MathUtils.cosDeg(playerBounds.getRotation() +180) * playerSpeed * GameScreen.deltaCff,
-                playerBounds.getY() + MathUtils.sinDeg(playerBounds.getRotation() + 180) * playerSpeed * GameScreen.deltaCff);
+    }
+    private  void playerMovement(){
+        playerBounds.setPosition(playerBounds.getX() + MathUtils.cosDeg(playerBounds.getRotation() +90) * playerSpeed * GameScreen.deltaCff,
+                playerBounds.getY() + MathUtils.sinDeg(playerBounds.getRotation() + 90) * playerSpeed * GameScreen.deltaCff);
+    }
+
+    private void playerSideMovement(){
+        playerBounds.setPosition(playerBounds.getX() + MathUtils.cosDeg(playerBounds.getRotation() ) * playerSpeed * GameScreen.deltaCff,
+                playerBounds.getY() + MathUtils.sinDeg(playerBounds.getRotation() ) * playerSpeed * GameScreen.deltaCff);
     }
     // гасим скорость
-    private void downSpeed() {
+    private void quenchSpeed() {
         if (playerSpeed > velosity* GameScreen.deltaCff)
             playerSpeed -= velosity* GameScreen.deltaCff;
         else if (playerSpeed < -velosity* GameScreen.deltaCff)
